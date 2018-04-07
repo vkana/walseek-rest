ce2b227e9e887c22631e0f3d389538d15d1401ef
vkana <dearvenumadhav@gmail.com>
2018-04-07 02:50:15 -0500

    CORS added

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const secrets = require('./secrets.json');
 let express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3001;
 
 const dbConnString = secrets.dbConnString;
 
-  mongoose = require('mongoose'),
+const  mongoose = require('mongoose'),
   Product = require('./api/models/productListModel'), //created model loading here
   bodyParser = require('body-parser');
 
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
+};
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConnString);
 
 
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
