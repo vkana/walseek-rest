765f2096666ecc7914d813d6fae032350c3569ff
vkana <dearvenumadhav@gmail.com>
2018-04-10 20:51:03 -0500

    save search functionality

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const  mongoose = require('mongoose'),
   Product = require('./api/models/productListModel'), //created model loading here
   bodyParser = require('body-parser');
 
-let allowCrossDomain = function(req, res, next) {
+let allowCrossDomain = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -21,18 +21,35 @@ let allowCrossDomain = function(req, res, next) {
       next();
     }
 };
+
+// let noCache = (req, res, next) => {
+//   req.headers['if-none-match'] = '';
+//   req.headers['if-modified-since'] = '';
+//   next();
+// }
+
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConnString);
 
 
 app.use(allowCrossDomain);
+app.get('/*', function(req, res, next){
+  res.setHeader('Last-Modified', (new Date()).toUTCString());
+  next();
+});
+//app.disable('etag');
+//app.use(noCache);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var routes = require('./api/routes/productListRoutes'); //importing route
-routes(app); //register the route
+var productListRoutes = require('./api/routes/productListRoutes'); //importing route
+var storePriceRoutes = require('./api/routes/storePriceListRoutes'); //importing route
+
+productListRoutes(app);
+storePriceRoutes(app); //register the route
 app.listen(port);
 
 console.log('walseek RESTful API server started on: ' + port);
