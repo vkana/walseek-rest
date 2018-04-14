aaf3a3c68a814253cececd9971d56a8ab065cea1
vkana <dearvenumadhav@gmail.com>
2018-04-14 01:08:04 -0500

    global variable fix

--- api/controllers/storePriceListController.js
+++ api/controllers/storePriceListController.js
@@ -2,8 +2,6 @@
 const axios = require('axios');
 const stores = require('../../stores.json');
 
-let item = {};
-
 const wmStoreSearch = (upc, store) => {
   let url = `https://search.mobile.walmart.com/v1/products-by-code/UPC/${upc}`;
   let storePrice = {
@@ -20,19 +18,15 @@ const wmStoreSearch = (upc, store) => {
       storePrice = {
         ...storePrice,
         price: data.inStore.price.priceInCents / 100,
-        stock: (data.inStore.inventory) ? data.inStore.inventory.status : 'NA'
+        stock: (data.inStore.inventory) ? data.inStore.inventory.status : 'NA',
+        //item
+        name: data.common.name,
+        sku: data.common.productId.wwwItemId,
+        upc: data.common.productId.upca,
+        url: data.common.productUrl,
+        bsUrl: `https://www.brickseek.com/walmart-inventory-checker?sku=${data.common.productId.wwwItemId}`,
+        offerType: data.common.offerType
       };
-
-      if (!item.sku) {
-        item = {
-          name: data.common.name,
-          sku: data.common.productId.wwwItemId,
-          upc: data.common.productId.upca,
-          url: data.common.productUrl,
-          bsUrl: `https://www.brickseek.com/walmart-inventory-checker?sku=${data.common.productId.wwwItemId}`,
-          offerType: data.common.offerType,
-        };
-      }
     }
 
     return storePrice;
@@ -43,8 +37,7 @@ const wmStoreSearch = (upc, store) => {
 
 const searchStores = async (upc, start, numStores) => {
   let allStores = stores.allStores;
-  let [storePrices, promiseArray, errorCount, item] = [[], [], 0, {}];
-
+  let [storePrices, promiseArray, errorCount] = [[], [], 0];
   allStores.slice(start, start + numStores).map(store => {
     promiseArray.push(wmStoreSearch(upc, store).catch(err=>{errorCount++}));
   });
@@ -63,10 +56,18 @@ exports.search_stores = async (req, res) => {
   let start = parseInt(req.query.start) || 0;
   let numStores = parseInt(req.query.stores) || 100;
   let storePrices = await searchStores(upc, start, numStores);
+  let item = {};
+  if (storePrices && storePrices.length >0) {
+    item = (({name, sku, upc, url, bsUrl, offerType}) => ({name, sku, upc, url, bsUrl, offerType}))(storePrices[0]);
+    storePrices = storePrices.map(s => {return {no:s.no, address: s.address, zip:s.zip, price:s.price, stock:s.stock}});
+  }
+  else {
+    storePrices=[];
+  }
+
   let resp = {
     item,
     storePrices
   };
-
   res.json(resp);
 };
