805ef1458d831b491f5da407100e7250f732d24b
vkana <dearvenumadhav@gmail.com>
2018-04-28 22:17:18 -0500

    added sku to upc

--- api/controllers/storePriceListController.js
+++ api/controllers/storePriceListController.js
@@ -1,6 +1,7 @@
 'use strict';
 const axios = require('axios');
 const stores = require('../../stores.json');
+const secrets = require('../../secrets.json');
 
 const wmStoreSearch = (upc, store) => {
   let url = `https://search.mobile.walmart.com/v1/products-by-code/UPC/${upc}`;
@@ -37,6 +38,10 @@ const wmStoreSearch = (upc, store) => {
 }
 
 const searchStores = async (upc, start, numStores) => {
+  if (upc.length < 12) {
+    upc = await getUPC(upc);
+  }
+
   let allStores = stores.allStores;
   let [storePrices, promiseArray, errorCount] = [[], [], 0];
   allStores.slice(start, start + numStores).map(store => {
@@ -51,6 +56,13 @@ const searchStores = async (upc, start, numStores) => {
 
   return storePrices;
 }
+
+const getUPC = async (sku) => {
+  const apiKey = secrets.apiKey;
+  const url = `https://api.walmartlabs.com/v1/items/${sku}?apiKey=${apiKey}`;
+  let resp = await axios.get(url);
+  return resp.data.upc;
+};
 
 exports.search_stores = async (req, res) => {
   let upc = req.params.productId;
