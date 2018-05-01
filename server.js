6f98846ba0660c63ae035ba75363b62de8fb18b8
vkana <dearvenumadhav@gmail.com>
2018-04-30 21:36:02 -0500

    trying CORS error fix

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const secrets = require('./secrets.json');
+const cors = require('cors');
 let express = require('express'),
   app = express(),
   port = process.env.PORT || 3001;
 
-let allowCrossDomain = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Length');
-    // intercept OPTIONS method
-    if ('OPTIONS' === req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-};
-app.use(allowCrossDomain);
+app.use(cors());
+// let allowCrossDomain = (req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Length');
+//     // intercept OPTIONS method
+//     if ('OPTIONS' === req.method) {
+//       res.sendStatus(200);
+//     }
+//     else {
+//       next();
+//     }
+// };
+// app.use(allowCrossDomain);
 
 
 const dbConnString = secrets.dbConnString;
