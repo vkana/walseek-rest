6c00fd7af507ca6dbd995267d8e647899a797ae8
vkana <dearvenumadhav@gmail.com>
2019-04-03 23:00:07 -0500

    code cleanup

--- api/controllers/productListController.js
+++ api/controllers/productListController.js
@@ -13,7 +13,6 @@ const getItemFromDb = async (sku) => {
 const getItemFromWm = async (sku) => {
   const apiKey = secrets.apiKey;
   const url = `https://api.walmartlabs.com/v1/items/${sku}?apiKey=${apiKey}`;
-  console.log(url);
   let response = await axios.get(url).catch(err => {
     console.log('error: ', err.response.status, err.response.statusText, sku);
     return err.response;
@@ -87,11 +86,9 @@ exports.get_upc = async (req, res) => {
   resp = await getItemFromDb(sku); 
   if (resp && resp.upc) {
     resp = {upc:resp.upc, variants: resp.variants || ''};
-    console.log('db ', resp);
   }
   else {
     resp = await getItemFromWm(sku);
-    console.log('wm ', resp);
   }
 
   res.json(resp);
