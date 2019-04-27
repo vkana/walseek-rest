e48449e21903cce04f885e6c6a75628b8bf45bff
vkana <dearvenumadhav@gmail.com>
2019-04-26 19:06:16 -0500

    min quantity added

--- api/controllers/storePriceListController.js
+++ api/controllers/storePriceListController.js
@@ -54,7 +54,7 @@ const mergeDetails = (storePrices, stores) => {
  });
 }
 
-const searchStores = async (upc, start, numStores, zip, inStockOnly) => {
+const searchStores = async (upc, start, numStores, zip, inStockOnly, minQty) => {
   let [storePrices, allStores, resultCount] = [[], [], 5];
 
   if (zip) {
@@ -69,10 +69,11 @@ const searchStores = async (upc, start, numStores, zip, inStockOnly) => {
   if (zip) {
     inStockStores = storePrices.filter(s => s.qty>0).map(s => {return s.no;});
   }
-  storePrices = storePrices.filter(s => s.price  && (inStockOnly ? (s.stock.startsWith('In') || s.stock.startsWith('Limited')) : true))
+  storePrices = storePrices.filter(s => s.price  
+    && (inStockOnly ? (s.stock.startsWith('In') || s.stock.startsWith('Limited')) : true)
+    && (minQty? s.qty >= minQty : true))
     .sort((a, b) => {return a.price - b.price})
     .slice(0, resultCount);
-
 
   let moreDetails = await getPickupTodayStatus(upc, storePrices);
   storePrices = mergeDetails(storePrices, moreDetails);
@@ -152,7 +153,8 @@ exports.search_stores = async (req, res) => {
   let numStores = parseInt(req.query.stores) || 10;
   let zip = parseInt(req.query.zip) || null;
   let inStockOnly = (req.query.inStockOnly && req.query.inStockOnly.toUpperCase() === 'TRUE') || false;
-  let storePrices = await searchStores(upc, start, numStores, zip, inStockOnly);
+  let minQty = parseInt(req.query.minqty);
+  let storePrices = await searchStores(upc, start, numStores, zip, inStockOnly, minQty);
   let item = {};
   if (storePrices && storePrices.length >0) {
     item = (({name, sku, upc, url, bsUrl, offerType, onlinePrice, stores}) => ({name, sku, upc, url, bsUrl, offerType, onlinePrice, stores}))(storePrices[0]);
